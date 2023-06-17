40aa3c660653905383a6d93d1a6d07e900f4d91a
Harry Nguyen <harry.nnh@gmail.com>
2023-06-17 10:25:18 +0200

    added comments

--- code/train.py
+++ code/train.py
@@ -2,16 +2,18 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, mean_squared_error
+from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_selection import SelectKBest, f_regression
 from sklearn.linear_model import LinearRegression
 from xgboost import XGBRegressor
 
+# Load the training and test datasets
 train_df = pd.read_csv("../data/allstate-claims-severity/train.csv")
 test_df = pd.read_csv("../data/allstate-claims-severity/test.csv")
+
 # Preprocessing data
-# Encoding categorical features to numeric
+# Encoding categorical features to numeric using LabelEncoder
 cat_columns_train = train_df.select_dtypes(include="object").columns
 cat_columns_test = test_df.select_dtypes(include="object").columns
 label_encoder = LabelEncoder()
@@ -22,52 +24,60 @@ test_df[cat_columns_test] = test_df[cat_columns_test].apply(
     label_encoder.fit_transform
 )
 
-
+# Extract features (X) and target variable (y) from the training dataset
 y = train_df["loss"].values
 X = train_df.drop("loss", axis=1).values
 
-
+# Extract features from the test dataset
 X_test = test_df.values
-# check na for features and target
+
+# Check for missing values
 print(np.any(np.isnan(np.sum(y))))
 print(np.any(np.isnan(np.sum(X))))
+
+# Compute correlation matrix
 corr_mat = np.corrcoef(X, rowvar=False)
 corr_threshold = 0.8
-# identify redundant features
+
+# Identify redundant features
 mask = np.abs(corr_mat) >= corr_threshold
 np.fill_diagonal(mask, False)
-# get indices of redundant features
 redundant_feature_indices = np.where(mask.any(axis=0))[0]
 print(redundant_feature_indices.shape)
+
+# Remove redundant features from X
 X_filtered = np.delete(X, redundant_feature_indices, axis=1)
-# feature selection on non redundant feature
+
+# Feature selection on non-redundant features using SelectKBest
 selector = SelectKBest(score_func=f_regression, k=102)
 X_selected = selector.fit_transform(X_filtered, y)
 selected_indices = selector.get_support(indices=True)
-print(f"selected features: {selected_indices}")
+print(f"Selected features: {selected_indices}")
 
-# select features in test dataset
+# Select corresponding features in the test dataset
 X_test_filtered = np.delete(X_test, redundant_feature_indices, axis=1)
 X_test_selected = selector.transform(X_test_filtered)
 
-
+# Split the data into training and validation sets
 X_train, X_val, y_train, y_val = train_test_split(
     X_selected, y, test_size=0.2, random_state=42
 )
-# simple model using OLS
+
+# Model training and evaluation
+# Linear Regression
 model = LinearRegression()
 model.fit(X_train, y_train)
 y_pred = model.predict(X_val)
-print(mean_squared_error(y_val, y_pred))
-print(mean_squared_error(y_val, y_pred, squared=False))
-# mse=2118
-# Extreme Gradient Boosting
+mse = mean_squared_error(y_val, y_pred)
+rmse = np.sqrt(mse)
+print(f"Linear Regression MSE: {mse}")
+print(f"Linear Regression RMSE: {rmse}")
 
+# XGBoost
 model_xgb = XGBRegressor(n_estimators=1000, learning_rate=0.05, max_depth=3)
 model_xgb.fit(X_train, y_train)
-y_pred_xgb = model_xgb.predict(
-    X_val,
-)
-print(mean_squared_error(y_pred_xgb, y_val))
-print(mean_squared_error(y_pred_xgb, y_val, squared=False))
-# mse=1098
+y_pred_xgb = model_xgb.predict(X_val)
+mse_xgb = mean_squared_error(y_val, y_pred_xgb)
+rmse_xgb = np.sqrt(mse_xgb)
+print(f"XGBoost MSE: {mse_xgb}")
+print(f"XGBoost RMSE: {rmse_xgb}")
